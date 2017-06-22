5103503749290be220143e3021ab50571dcf5344
Rahul <rkredux@gmail.com>
2017-06-22 06:05:21 -0400

    Change class name and remove the p tag from home.js

--- src/components/home.js
+++ src/components/home.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const Home = () => {
 	return (
-		<div className="content"><p>This is home page</p></div>
+		<div className="home-content">
+           
+          
+		</div>
 		); 
 }
 
