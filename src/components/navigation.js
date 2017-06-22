34fbbb444cf2d552dd36c2602765e8bc6289e54d
Rahul <rkredux@gmail.com>
2017-06-22 06:06:49 -0400

    Add Logo and add more nav tabs to the bar.

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -7,12 +7,26 @@ class Navigation extends React.Component{
 		return (
 
 		<nav className="flex-nav">
-		  <ul className="navbar">
-            <li className="home tab"><Link to="/">ABOUT</Link></li>
-            <li className="stuff tab"><Link to="/stuff">PORTFOLIO</Link></li>
-            <li className="contact tab"><Link to="/contact">CONTACT</Link></li>
-          </ul>
-        </nav>
+
+       
+
+           <div className="logo"><i className="fa fa-paw" aria-hidden="true"></i><span>DOGFRENCE</span></div>
+
+    		   <ul className="navbar">
+                
+                <li className="home tab"><Link to="/">ABOUT</Link></li>
+                <li className="stuff tab"><Link to="/stuff">SPEAKERS</Link></li>
+                <li className="contact tab"><Link to="/contact">SCHEDULE</Link></li>
+                <li className="contact tab"><Link to="/contact">PRICING</Link></li>
+                <li className="contact tab"><Link to="/contact">VENUE</Link></li>
+
+           </ul>
+
+
+
+
+    </nav>
+
          ); 
 	}
 }
