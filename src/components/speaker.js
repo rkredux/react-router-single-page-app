db2998837ffcb05b5cbcd0a86121a203e6c3b6ac
Rahul <rkredux@gmail.com>
2017-06-23 13:18:53 -0400

    Import Speaker profile and loop over the object to create markups of speaker profiles.

---
+++ src/components/speaker.js
@@ -0,0 +1,16 @@
+import React from "react";
+import SampleSpeakers from "./SampleSpeakers.js"; 
+import Image from "./image.js";
+
+
+const Speakers = () => {
+	return (
+		<div className="main-content">
+		  <div className="inner-wrapper speaker">
+		    {Object.keys(SampleSpeakers).map((key) => <Image breed={SampleSpeakers[key].breed} name={SampleSpeakers[key].name} path={require(SampleSpeakers[key].image)} key={key} index={key}/>)}
+		  </div>
+		</div>
+		); 
+}
+
+export default Speakers; 
