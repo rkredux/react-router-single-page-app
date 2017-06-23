f52ec80db60bfac10f8e8853152e358f84a92516
Rahul <rkredux@gmail.com>
2017-06-23 13:14:36 -0400

    Changes in main.js navigation and index.css to include design for Image component.

--- src/components/main.js
+++ src/components/main.js
@@ -3,17 +3,25 @@ import {Switch, Route} from "react-router-dom";
 import Home from "./home.js"; 
 import Stuff from "./stuff.js"; 
 import Contact from "./contact.js"; 
+import Speakers from "./speaker.js"; 
+import Schedule from "./schedule.js"; 
+import Pricing from "./pricing.js"; 
+import Venue from "./venue.js"; 
+
+
 
 
 
 class Main extends React.Component{
-	
+
 	render(){
 		return (
 		<Switch>
 			<Route exact path="/" component={Home}></Route>
-			<Route path="/stuff" component={Stuff}></Route>
-			<Route path="/contact" component={Contact}></Route>
+			<Route path="/speakers" component={Speakers}></Route>
+			<Route path="/schedule" component={Schedule}></Route>
+			<Route path="/pricing" component={Pricing}></Route>
+			<Route path="/venue" component={Venue}></Route>
 		</Switch>	
 			
          ); 
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -15,10 +15,10 @@ class Navigation extends React.Component{
     		   <ul className="navbar">
                 
                 <li className="home tab"><Link to="/">ABOUT</Link></li>
-                <li className="stuff tab"><Link to="/stuff">SPEAKERS</Link></li>
-                <li className="contact tab"><Link to="/contact">SCHEDULE</Link></li>
-                <li className="contact tab"><Link to="/contact">PRICING</Link></li>
-                <li className="contact tab"><Link to="/contact">VENUE</Link></li>
+                <li className="stuff tab"><Link to="/speakers">SPEAKERS</Link></li>
+                <li className="contact tab"><Link to="/schedule">SCHEDULE</Link></li>
+                <li className="contact tab"><Link to="/pricing">PRICING</Link></li>
+                <li className="contact tab"><Link to="/venue">VENUE</Link></li>
 
            </ul>
 
