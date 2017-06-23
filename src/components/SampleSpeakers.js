2b1f49c44f4edad55d27708caeeb1b637366aabb
Rahul <rkredux@gmail.com>
2017-06-23 13:15:38 -0400

    Add list of speakers in the conference

---
+++ src/components/SampleSpeakers.js
@@ -0,0 +1,49 @@
+// This is just some sample data so you don't have to think of your own!
+
+const SampleSpeakers = {
+
+  speaker1: {
+    name: 'Pacific Halibut',
+    image: "./conference-images/conference-dog1.jpg", 
+    desc: 'Everyones favorite white fish. We will cut it to the size you need and ship it.', 
+    breed: "bulldog"
+  },
+
+  speaker2: {
+    name: 'Lobster',
+    image: "./conference-images/conference-dog2.jpg", 
+    desc: 'These tender, mouth-watering beauties are a fantastic hit at any dinner party.', 
+    breed: "bulldog"
+  },
+
+  speaker3: {
+    name: 'Sea Scallops',
+    image: "./conference-images/conference-dog3.jpg", 
+    desc: 'Big, sweet and tender. True dry-pack scallops from the icey waters of Alaska. About 8-10 per pound.', 
+    breed: "bulldog"
+  },
+
+  speaker4: {
+    name: 'Mahi Mahi',
+    image: "./conference-images/conference-dog4.jpg", 
+    desc: 'Lean flesh with a mild, sweet flavor profile, moderately firm texture and large, moist flakes.', 
+    breed: "bulldog"
+  },
+
+  speaker5: {
+    name: 'King Crab',
+    image: "./conference-images/conference-dog5.jpg", 
+    desc: 'Crack these open and enjoy them plain or with one of our cocktail sauces.', 
+    breed: "bulldog"
+  },
+
+  speaker6: {
+    name: 'Atlantic Salmon',
+    image: "./conference-images/conference-dog6.jpg", 
+    desc: 'This flaky, oily salmon is truly the king of the sea. Bake it, grill it, broil it...as good as it gets!', 
+    breed: "bulldog"
+  }
+
+};
+
+export default SampleSpeakers; 
