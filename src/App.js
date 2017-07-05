8db612dbc103750b8c9d84a0449f20a7031d55aa
Rahul <rkredux@gmail.com>
2017-07-04 22:20:46 -0400

    Just some minor edits.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import './App.css';
 import Navigation from "./components/navigation.js"; 
 import Main from "./components/main.js"; 
+import NavBar from "./components/NavBar.js"; 
 
 
 
 class App extends React.Component{
+  
+
   render(){
     return (
       <div className="wrapper">
-        <Navigation/>
+        <Navigation/>   
         <Main/>
       </div>
       ); 
