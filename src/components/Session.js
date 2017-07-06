50d5f5b9e39d48d78deb6aea2a55cc5a5d5849b4
Rahul <rkredux@gmail.com>
2017-07-06 15:37:34 -0400

    Add CSSTransition group

--- src/components/Session.js
+++ src/components/Session.js
@@ -1,15 +1,29 @@
 import React from "react"; 
+import { CSSTransitionGroup } from "react-transition-group"; 
+
  
 
 class Session extends React.Component {	
 		
 		render(){
+
+
+
 			return (	
-			      <ul className="session-container">
-			      	<li className="session time">{this.props.time}</li>
-			      	<li className="session name">{this.props.name}</li>
-			      	<li className="session speaker-name">{this.props.speaker}</li>
-			      </ul>
+
+
+		         <CSSTransitionGroup
+		          transitionName="example"
+		          transitionAppear={true}
+		          transitionAppearTimeout={500}
+		          transitionEnter={false}
+		          transitionLeave={false}>	
+				      <ul className="session-container">
+				      	<li className="session time">{this.props.time}</li>
+				      	<li className="session name">{this.props.name}</li>
+				      	<li className="session speaker-name">{this.props.speaker}</li>
+				      </ul>
+				 </CSSTransitionGroup>
 
 		    ); 
         }	
@@ -19,3 +33,6 @@ class Session extends React.Component {
 
 
 export default Session; 
+
+
+
