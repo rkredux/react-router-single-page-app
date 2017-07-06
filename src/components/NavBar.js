aa7d62c5ad3b87eff59053e6a87618a7470017f1
Rahul <rkredux@gmail.com>
2017-07-06 15:36:09 -0400

    Import and add NavLink component with activeStyle prop to keep the active link highlighted.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,30 @@
 import React from "react"; 
-import {Link} from 'react-router-dom'; 
+// import {Link} from 'react-router-dom'; 
+import { NavLink } from "react-router-dom"; 
 
 
 
 class NavBar extends React.Component{
 
+
 	render(){
+
+		const elementClickStyle = {
+			color: "black", 
+			background: "white"
+		}; 
 
 		return (
 		    <ul className="navbar">               
-	                <li className="home tab"><Link to="/">ABOUT</Link></li>
-	                <li className="stuff tab"><Link to="/speakers/">SPEAKERS</Link></li>
-	                <li className="contact tab"><Link to="/schedule/">SCHEDULE</Link></li>
-	                <li className="contact tab"><Link to="/pricing">PRICING</Link></li>
-	                <li className="contact tab"><Link to="/venue">VENUE</Link></li>
+	                <li className="home tab"><NavLink exact activeStyle={elementClickStyle} to="/">ABOUT</NavLink></li>
+	                <li className="stuff tab"><NavLink activeStyle={elementClickStyle} to="/speakers/">SPEAKERS</NavLink></li>
+	                <li className="contact tab"><NavLink activeStyle={elementClickStyle} to="/schedule/">SCHEDULE</NavLink></li>
+	                <li className="contact tab"><NavLink activeStyle={elementClickStyle} to="/pricing">PRICING</NavLink></li>
 	        </ul>
-	    ); 
-
+        )
     }
-}
 
+}
 
 export default NavBar; 
 
